9993842fb7400e639b8314a8dd596903ac2f5bf4
Katherine Albany <git@kalbany.net>
2016-02-21 09:23:05 +0000

    Checkpoint

--- logger/logger.go
+++ logger/logger.go
@@ -2,10 +2,34 @@ package logger
 
 import "fmt"
 
-type Logger interface {
-	Log()
+var Level int
+
+func Debug(message string) {
+	if Level >= 0 {
+		fmt.Println("[DEBUG]", message)
+	}
 }
 
-func Log(level int, message string) {
-	fmt.Println(message)
+func Info(message string) {
+	if Level >= 1 {
+		fmt.Println("[INFO ]", message)
+	}
+}
+
+func Warn(message string) {
+	if Level >= 2 {
+		fmt.Println("[WARN ]", message)
+	}
+}
+
+func Error(message string) {
+	if Level >= 3 {
+		fmt.Println("[ERROR]", message)
+	}
+}
+
+func Fatal(message string) {
+	if Level >= 5 {
+		fmt.Println("[FATAL]", message)
+	}
 }
--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "github.com/katherinealbany/rodentia/logger"
 
 func main() {
-	logger.Log(1, "level 1")
-	logger.Log(2, "level 2")
-	logger.Log(3, "level 3")
-	logger.Log(4, "level 4")
-	logger.Log(5, "level 5")
+	logger.Level = 1
+	logger.Debug("level 1")
+	logger.Info("level 2")
+	logger.Warn("level 3")
+	logger.Error("level 4")
+	logger.Fatal("level 5")
 }
