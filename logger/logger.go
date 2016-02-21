bdbe9b551fd82e72cc71f63103895adb134733cb
Katherine Albany <git@kalbany.net>
2016-02-21 10:17:21 +0000

    Checkpoint

--- logger/logger.go
+++ logger/logger.go
@@ -7,37 +7,47 @@ import (
 
 var Level int
 
+var start time.Time
+
+func init() {
+	start = time.Now()
+}
+
 func Debug(message string) {
 	if Level >= 0 {
-		fmt.Println(timestamp(), "[DEBUG]", message)
+		fmt.Println(timestamp(), since(), "[DEBUG]", message)
 	}
 }
 
 func Info(message string) {
 	if Level >= 1 {
-		fmt.Println(timestamp(), "[INFO ]", message)
+		fmt.Println(timestamp(), since(), "[INFO ]", message)
 	}
 }
 
 func Warn(message string) {
 	if Level >= 2 {
-		fmt.Println(timestamp(), "[WARN ]", message)
+		fmt.Println(timestamp(), since(), "[WARN ]", message)
 	}
 }
 
 func Error(message string) {
 	if Level >= 3 {
-		fmt.Println(timestamp(), "[ERROR]", message)
+		fmt.Println(timestamp(), since(), "[ERROR]", message)
 	}
 }
 
 func Fatal(message string) {
 	if Level >= 5 {
-		fmt.Println(timestamp(), "[FATAL]", message)
+		fmt.Println(timestamp(), since(), "[FATAL]", message)
 	}
 }
 
 func timestamp() (timestamp string) {
 	t := time.Now()
-	return "[" + string(t.String()) + "]"
+	return "[" + t.Format("15:04:05.000") + "]"
+}
+
+func since() (timestamp string) {
+	return "[" + time.Since(start).String() + "]"
 }
