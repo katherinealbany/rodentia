1a5e0edb4a89e7a4b4dca8bdfa2c2aaa0eb15701
Katherine Albany <git@kalbany.net>
2016-02-25 08:08:19 +0000

    Checkpoint

--- mediation/input.go
+++ mediation/input.go
@@ -4,15 +4,24 @@ import (
 	"fmt"
 	"github.com/katherinealbany/rodentia/logger"
 	"io/ioutil"
+	"os"
 )
 
 var log = logger.New("mediation")
 
 func Read() {
 	log.Info("Reading ...")
+
 	data, err := ioutil.ReadFile("./test.csv")
 	if err != nil {
 		panic(err)
 	}
 	fmt.Print(string(data))
+
+	file, err := os.Open("./test.csv")
+	if err != nil {
+		fmt.Println("error:", err)
+		// log.Fatal("error:" + err)
+	}
+	defer file.Close()
 }
